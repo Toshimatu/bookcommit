29b4d104a869db8c28626383c6f79118e1edde30
Akitoshi <matsuda.akt@gmail.com>
2019-08-24 05:13:02 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+#「Homeページのテスト。GETリクエストをhomeアクションに対して発行 (=送信) せよ
+#そうすれば、リクエストに対するレスポンスは[成功]になるはず」
+#という意味
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -10,5 +14,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
   end
-
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+  
+    test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
 end
