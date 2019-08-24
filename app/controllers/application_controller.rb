1e4649eeacb409e5c6109e5df64db91fe71546cc
Akitoshi <matsuda.akt@gmail.com>
2019-08-24 02:06:36 +0000

    Initialize repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "とりあえず最初のページ作成"
+    render html: "最初のページ"
   end
+  
   
 end
